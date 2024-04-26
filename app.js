41e3443b799a5ffad255d8fe502d12c30c2bf786
iremsahin9 <iremsahinq@gmail.com>
2024-04-26 13:42:13 +0300

    javascript_examples

---
+++ app.js
@@ -0,0 +1,66 @@
+// -------------DERS ORTALAMASI BULMA-----------------------------
+
+// let vize = Number(prompt("Vize notunuz:"));
+// let final = Number(prompt("Final notunuz:"));
+
+// let ortalama= (vize*0.4) + (final*0.6)
+
+// console.log(ortalama);
+
+// if(ortalama>50){
+// console.log("Geçtiniz"+ortalama);
+// }else{
+// console.log("Kaldınız:  "+ortalama)
+// }
+
+//---------------YOL AYRIMI UYGULAMASI-------------------------------------
+
+/*
+
+-                  -                 -
+ -                 -                - 
+   -               -               -  
+    -              -              -
+ 1.yol            2.yol          3.yol    
+
+*/
+
+// let secilenyol= prompt("Seçilen yolu giriniz: ");
+
+// if(secilenyol==1){
+//       alert("seçilen yol  "+secilenyol+"'dir.");
+// }
+// else if(secilenyol==2){
+//     alert("seçilen yol "+secilenyol+"'dir.");
+// }
+// else if(secilenyol==3){
+//     alert("seçilen yol  "+secilenyol+"'dür.");
+// }
+// else{
+//     alert("Geçerli bir yol giriniz,  yol seçeneğiniz 3 seçenektir.")
+// }
+
+
+//-----------------------BEDEN KİTLE ENDEKSİ HESAPLAMA--------------------------------
+
+// let kg = Number(prompt("Kilonuzu giriniz:"));
+// let boy =Number(prompt("Boyunuzu metre cinsinden giriniz:"))
+
+// let endeks = kg/(boy*2)
+
+// if(indeks<18.5 ){
+// alert("İdeal kilonun altında: "+endeksndeks);
+// }
+// else if(endeksndeks>=18.5 && endeks<=24.9){
+//     alert(" İdeal kiloda: "+endeksndeks);  
+// }
+// else if(endeks>=25 && endeksndeks<=29.9){
+//     alert(" İdeal kilonun üstünde: "+endeksndeks);  
+// }
+// else if(endeks>=30 && endeks<=39.9){
+//     alert(" İdeal kilonun çok üstünde (obez): "+endeks);  
+// }
+// else if (endeks>=40){
+//     alert(" İdeal kilonun çok üstünde (morbid obez): "+endeks);  
+// }
+//\r\n=yeni satır yaratır
